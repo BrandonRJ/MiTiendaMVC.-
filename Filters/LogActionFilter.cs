1f67c23869965cc154ec2ea2f7ae403433127a60
Brandon Daniel Robles Jimenez <brandon-95cr@hotmail.com>
2026-03-25 12:57:49 -0600

    Creacion de la carpeta Filters/LogActionFilter.cs y su logica.

---
+++ Filters/LogActionFilter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Diagnostics;
+using System.Web.Mvc;
+
+namespace MiTiendaMVC.Filters
+{
+    // Heredamos de ActionFilterAttribute para poder sobrescribir sus métodos
+    public class LogActionFilter : ActionFilterAttribute
+    {
+        // Este método se ejecuta antes de que la acción del controlador inicie
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            string accion = filterContext.ActionDescriptor.ActionName;
+            string fechaHora = DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss");
+
+            // Imprime en la ventana de "Salida" (Output) de Visual Studio
+            Debug.WriteLine($"[LOG] Acción: {accion} | Ejecutada el: {fechaHora}");
+
+            base.OnActionExecuting(filterContext);
+        }
+    }
+}
