1a1c91d1e10ac5f94b04a2d7622c9f754cedf8f4
Brandon Daniel Robles Jimenez <brandon-95cr@hotmail.com>
2026-03-25 13:39:10 -0600

    Modificacion de carpeta App_Start y craecion de clase BundleConfig,cs y su logica para conectar con estilos.

---
+++ App_Start/BundleConfig.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace MiTiendaMVC
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            // Creamos un bundle para nuestros estilos CSS
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/Site.css"));
+
+            // Habilitar la optimización (minificación)
+            BundleTable.EnableOptimizations = true;
+        }
+    }
+}
